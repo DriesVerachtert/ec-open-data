75c20956103433030210ead9e27a4698c0be3f5f
Dries Verachtert <dries.verachtert@dries.eu>
2015-05-25 22:49:35 +0200

    some first graphs with DG EAC open data

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ec-open-data
-Statistics derived from some of the open data of the European Commission
+This repository contains some graphs and statistics that are derived from the 
+[open data of the European Commission](http://open-data.europa.eu/data/).
+
+The [European Commission](http://ec.europa.eu/) already publishes lots of
+information and statistics on their open data website, but quite a lot is stored in PDF files
+which makes (automated) reuse not so easy. This repository contains some basic
+statistics in easily reusable formats like [TSV](http://en.wikipedia.org/wiki/Tab-separated_values)
+and [JSON](http://en.wikipedia.org/wiki/JSON) with visualisations in a mix
+of [HTML](http://en.wikipedia.org/wiki/HTML) and
+[SVG](http://en.wikipedia.org/wiki/Scalable_Vector_Graphics).
+
+At the moment it's only based on open data of the [Directorate General for
+Education and Culture (DG EAC)](http://ec.europa.eu/dgs/education_culture/index_en.htm)
+
+**[View some generated graphs](dg_eac/out/index.html)**
---
+++ dg_eac/run.rb
@@ -0,0 +1,83 @@
+require 'csv'
+require 'json'
+
+# From http://open-data.europa.eu/data/dataset/erasmus-mobility-statistics-2012-13
+# Raw data of Erasmus student mobility (study exchanges and work placements in 2012-13) 
+#studmobcsv = 'input/SM_2012_13_20141103_01.partial.csv' # partial input for quick testing
+studmobcsv = 'input/SM_2012_13_20141103_01.csv'
+
+homeCountryToNumberOfParticipants = Hash.new
+hostCountryToNumberOfParticipants = Hash.new
+countryToHomeToHostRatio = Hash.new
+homeCountryToNumberOfMaleParticipants = Hash.new
+homeCountryToNumberOfFemaleParticipants = Hash.new
+hostCountryToNumberOfMaleParticipants = Hash.new
+hostCountryToNumberOfFemaleParticipants = Hash.new
+
+CSV.foreach(studmobcsv, :headers => true , :encoding => 'ISO-8859-1', :quote_char => '"', :col_sep => ';') do |row|
+  # hostC = host country
+  hostC = row[12] # for normal students
+  if hostC == '???' then # staff (i think) doesn't have a host institution and country
+    hostC = row[14] # take the country of the enterprise
+    if hostC == nil then # 3 lines with bad data
+      hostC = '???'
+    end
+  end
+  # homeC = home country
+  homeC = row[3]
+  gender = row[5]
+  age = row[6]
+  
+  homeCountryToNumberOfParticipants[homeC] = homeCountryToNumberOfParticipants.fetch(homeC,0) + 1
+  hostCountryToNumberOfParticipants[hostC] = hostCountryToNumberOfParticipants.fetch(hostC,0) + 1
+  countryToHomeToHostRatio[homeC] = countryToHomeToHostRatio.fetch(homeC,0) + 1
+  countryToHomeToHostRatio[hostC] = countryToHomeToHostRatio.fetch(hostC,0) - 1
+  if gender == 'M' then
+    homeCountryToNumberOfMaleParticipants[homeC] = homeCountryToNumberOfMaleParticipants.fetch(homeC,0) + 1
+    hostCountryToNumberOfMaleParticipants[hostC] = hostCountryToNumberOfMaleParticipants.fetch(hostC,0) + 1
+  end
+  if gender == 'F' then
+    homeCountryToNumberOfFemaleParticipants[homeC] = homeCountryToNumberOfFemaleParticipants.fetch(homeC,0) + 1
+    hostCountryToNumberOfFemaleParticipants[hostC] = hostCountryToNumberOfFemaleParticipants.fetch(hostC,0) + 1
+  end
+end
+
+def createOneToOneGraph(name, hash, keyname, valname, description, indexPage)
+  tsvfilename = 'out/' + name + '.tsv'
+  f = File.open(tsvfilename,'w')
+  f << keyname + "\t" + valname + "\n" + hash.keys.sort.map {|c| c + "\t" + hash[c].to_s }.join("\n")
+  f.close
+  jsonfilename = 'out/' + name + '.json'
+  f = File.open(jsonfilename,'w')
+  f << hash.to_json
+  f.close
+  s = IO.read('html/oneToOneTemplate2.html')
+  s = s.gsub('__YAXIS__',valname).gsub('__XAXIS__',keyname).gsub('__DESCRIPTION__',description).gsub('__TSVFILENAME__',name + '.tsv')
+  f = File.open('out/' + name + '.html','w')
+  f << s
+  f.close
+  indexPage << '<h2>' + description + '</h2>'
+  indexPage << '<h3>Raw data</h3>'
+  indexPage << '<ul><li><a href="' + name + '.tsv">' + name + '.tsv</a></li>'
+  indexPage << '<li><a href="' + name + '.json">' + name + '.json</a></li></ul>'
+  indexPage << '<h3>Graph</h3>'
+  indexPage << '<iframe width="1150" height="650" src="' + name + '.html"></iframe>'
+end
+
+indexPage = StringIO.new
+indexPage << '<h1>Erasmus student mobility (study exchanges and work placements in 2012-13)</h1>'
+indexPage << 'Based on <a href="http://open-data.europa.eu/data/dataset/erasmus-mobility-statistics-2012-13">the raw data of Erasmus student mobility (study exchanges and work placements in 2012-13)</a>'
+
+createOneToOneGraph('homeCountryToNumberOfParticipants', homeCountryToNumberOfParticipants, 'country', 'participants', 'Number of participants per home country', indexPage)
+createOneToOneGraph('hostCountryToNumberOfParticipants', hostCountryToNumberOfParticipants, 'country', 'participants', 'Number of participants per host country', indexPage)
+createOneToOneGraph('countryToHomeToHostRatio', countryToHomeToHostRatio, 'country', 'ratio', 'Number of outgoing minus the number of incoming participants per country', indexPage)
+createOneToOneGraph('homeCountryToNumberOfMaleParticipants', homeCountryToNumberOfMaleParticipants, 'country', 'participants', 'Number of male participants per home country', indexPage)
+createOneToOneGraph('homeCountryToNumberOfFemaleParticipants', homeCountryToNumberOfFemaleParticipants, 'country', 'participants', 'Number of female participants per home country', indexPage)
+createOneToOneGraph('hostCountryToNumberOfMaleParticipants', hostCountryToNumberOfMaleParticipants, 'country', 'participants', 'Number of male participants per host country', indexPage)
+createOneToOneGraph('hostCountryToNumberOfFemaleParticipants', hostCountryToNumberOfFemaleParticipants, 'country', 'participants', 'Number of female participants per host country', indexPage)
+
+s = IO.read('html/indexPageTemplate.html')
+s = s.gsub('__CONTENTS__', indexPage.string)
+f = File.open('out/index.html','w')
+f << s
+f.close
